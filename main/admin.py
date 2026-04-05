481de9f63559f10f3c216a9c40202eb823a0812c
Vitalii <ananievvitalii10@gmail.com>
2026-04-04 17:12:49 -0700

    Registered the CompanyProfile and TeamMember models in admin.py for management through the custom admin site.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.admin import custom_admin_site
+from main.models import CompanyProfile, TeamMember
+
+
+@admin.register(CompanyProfile, site=custom_admin_site)
+class CompanyProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active")
+    fields = (
+        "name",
+        "primary_phone",
+        "secondary_phone",
+        "email",
+        "address",
+        "working_hours",
+        "logo",
+        "years_on_market",
+        "completed_objects_count",
+        "employees_count",
+        "about",
+        "facebook_url",
+        "instagram_url",
+        "telegram_url",
+        "is_active",
+    )
+
+
+@admin.register(TeamMember, site=custom_admin_site)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ("employee", "display_order", "is_visible")
+    list_filter = ("is_visible",)
+    search_fields = ("employee__first_name", "employee__last_name")
+    fields = (
+        "employee",
+        "description",
+        "photo",
+        "display_order",
+        "is_visible",
+    )
