da2d5b2ba77138f2c497df1c4ec320048613d067
Vitalii <ananievvitalii10@gmail.com>
2026-04-18 09:59:52 -0700

    setup email via Brevo using Anymail
    - added django-anymail
    - configured Brevo backend
    - removed old SMTP settings
    - added new environment variables for Brevo

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     "user",
     "account",
     "axes",
+    "anymail",
     "crispy_forms",
     "crispy_bootstrap5",
     "debug_toolbar",
@@ -116,12 +117,11 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ["EMAIL_HOST"]
-EMAIL_PORT = int(os.environ["EMAIL_PORT"])
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+ANYMAIL = {
+    "BREVO_API_KEY": os.environ.get("BREVO_API_KEY"),
+}
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 COMPANY_CONTACT_EMAIL = os.getenv("COMPANY_CONTACT_EMAIL")
 
