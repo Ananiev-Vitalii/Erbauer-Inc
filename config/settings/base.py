50f0294d4ca0145a016be9d9d3e675a01cec0ebe
Vitalii <ananievvitalii10@gmail.com>
2026-04-28 10:33:21 -0700

    Switch to StaticFilesStorage to fix collectstatic errors

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,10 +110,10 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
