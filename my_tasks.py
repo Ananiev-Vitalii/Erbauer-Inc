1cb2a66a0af449a4cfe4d524717527d73c18e28c
Vitalii <ananievvitalii10@gmail.com>
2026-04-27 19:02:36 -0700

    Remove missing Bootstrap source map reference

---
+++ my_tasks.py
@@ -0,0 +1,45 @@
+
+
+"""
+Проверить баг с переходом view details -> in section project and
+project_list category/paginator for mobile
+
+
+
+- render,
+- после развертывания добавить в https://app.brevo.com/ домен а в captcha хост
+- медиа
+
+- посмотреть приложения и миделвеа которіми ми не пользуемся и удалить их
+- env_exemple
+-tests
+-README
+- возможно в файл build.sh добавить проверку тестов
+
+
+
+
+
+// --- MAIN --- //
+// --- USER --- //
+// --- PROFILE --- //
+
+"""
+
+"""
+Cursor/Zed/Trae
+services
+    - __init__.py
+    - infrastructure += [token, send_message_from_email, auth/rite_limit, register/rite_limit]
+      use UserEntity dataclass for User model
+    - domain += [django.contrib.user]
+"""
+
+"""
+WhiteNoise — что это и зачем?
+Это библиотека для Django, она нужна потомучто в продакшене Django
+сам по себе не раздает static-файлы
+Middleware — зачем? 
+Это говорит Django: если пришёл запрос на /static/... → отдай файл
+STORAGES для кеширования.
+"""
